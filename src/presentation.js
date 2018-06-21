bb1e3412fa897a14291d94c65328128ae00370d2
Petr Čaněk <canekpet@gmail.com>
2018-06-21 13:54:59 +0200

    Added prettier

--- src/presentation.js
+++ src/presentation.js
@@ -1,7 +1,5 @@
-// Import React
 import React from 'react';
 
-// Import Spectacle Core tags
 import {
   BlockQuote,
   Cite,
@@ -14,33 +12,27 @@ import {
   Text,
 } from 'spectacle';
 
-// Import theme
 import createTheme from 'spectacle/lib/themes/default';
 
-// Require CSS
 require('normalize.css');
 
 const theme = createTheme(
   {
     primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quartenary: '#CECECE',
+    secondary: '#1F2022', // Black
+    tertiary: '#03A9FC', // Blue
+    quarternary: '#CECECE', // Gray
   },
   {
     primary: 'Montserrat',
     secondary: 'Helvetica',
-  }
+  },
 );
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-      >
+      <Deck transitionDuration={500} theme={theme}>
         <Slide transition={['zoom']} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Spectacle Boilerplate
